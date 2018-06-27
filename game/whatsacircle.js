a74dade45ab0bebe0f9b9e5f3d0daa55e8a90205
hen1227 <32469043+hen1227@users.noreply.github.com>
2018-06-27 14:05:47 -0400

    Update whatsacircle.js

--- game/whatsacircle.js
+++ game/whatsacircle.js
@@ -4,6 +4,7 @@ var centery = 0;
 var averagedist = 0;
 var iscircle = false;
 var drawn = false; 
+var Visual = false;
 
 function setup() {
   createCanvas(400, 400);
@@ -24,14 +25,21 @@ function draw() {
     }
 		centerx = centerx / placed.length;
 		centery = centery / placed.length;
-		// print(centerx, centery);
+      strokeWeight(1);
     fill (255,0,0);
-    strokeWeight(1);
 		ellipse(centerx, centery, 10 ,10);
     for(var k = 0; k < placed.length; k++){
       averagedist += dist(placed[k].x, placed[k].y, centerx, centery);
     }
     averagedist = averagedist / placed.length;
+    	noFill();
+      strokeWeight(1);
+    if(Visual){
+  	  //ellipse(centerx, centery, averagedist-averagedist+placed.length/9, averagedist-averagedist+placed.length/9);
+  	  ellipse(centerx, centery, averagedist+averagedist+placed.length/9, averagedist+averagedist+placed.length/9);
+    }
+    fill(255,0,0);
+      
      iscircle = true;
     for(var l = 0; l < placed.length; l++){
       if(dist(placed[l].x, placed[l].y, centerx, centery) < averagedist+placed.length/9 && dist(placed[l].x, placed[l].y, centerx, centery) > averagedist-placed.length/9){}else{
@@ -65,7 +73,9 @@ function mouseDragged(){
 }
 
 function mouseReleased(){
-	drawn = true;
+  if(dist(mouseX, mouseY, width/2, height/2) < width/2){
+		drawn = true;
+	}
 }
 function dot(mex, mey){
 	this.x = mex;
@@ -75,4 +85,15 @@ function dot(mex, mey){
 		strokeWeight(11);
 		point(this.x, this.y);
 	}
+}
+
+
+function toggleVisual(){
+	if(Visual){
+  	Visual = false;
+    document.getElementById("VisualMode").innerHTML = "Off"; 
+  }else{
+  	Visual = true;
+    document.getElementById("VisualMode").innerHTML = "On"; 
+  }
 }
